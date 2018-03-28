4bb52baa1855620dd53eb7440adaf1b803fe8ecb
Oliver Flatt <Oliver Flatt>
2018-03-28 12:51:14 -0600

    delete extra files

--- README.md
+++
@@ -1,4 +0,0 @@
-# the-dorito2-theif
-the theif of doritos
-
-The game which space invaders was based off of.
--- sounds.py
+++
@@ -1,6 +0,0 @@
-import pygame
-from pygame.mixer import Sound
-
-pygame.mixer.init()
-
-mutecity = Sound("mutecity.wav") 
